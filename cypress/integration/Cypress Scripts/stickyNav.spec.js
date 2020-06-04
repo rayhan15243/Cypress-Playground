572df8856405382067be6ade3ab0032eb5afb2ef
Chowdhury <k1786825@kcl.ac.uk>
2020-06-04 14:28:18 +0100

    Hello

---
+++ cypress/integration/Cypress Scripts/stickyNav.spec.js
@@ -0,0 +1,16 @@
+describe("Sticky Nav", function(){
+
+    it('Sections', function() {
+        cy.visit("http://storybook.develop.kcl.contensis.cloud/iframe.html?id=components-stickynav--with-scroll-to#section-1")
+            cy.contains('Section 1').should('be.visible').click()
+            cy.contains('Section 2').should('be.visible').click()
+            cy.contains('Section 3').should('be.visible').click()
+            cy.contains('Section 4').should('be.visible').click()
+            cy.contains('Section 5').should('be.visible').click()
+            cy.contains('Section 6').should('be.visible').click()
+            cy.scrollTo(0, 500)    
+  })
+
+ 
+
+})
