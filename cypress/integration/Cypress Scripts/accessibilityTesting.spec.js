eedc7c7b7fed7808abc66ec245da90f1b6e2f639
Rayhan Ahmed Chowdhury <36962596+rayhan15243@users.noreply.github.com>
2020-06-22 14:09:01 +0100

    Added 00,00 * * * *

--- cypress/integration/Cypress Scripts/accessibilityTesting.spec.js
+++ cypress/integration/Cypress Scripts/accessibilityTesting.spec.js
@@ -1,14 +1,10 @@
 
 describe("Accessibility Demo", function(){
 
-    beforeEach(() => {
-
-        cy.visit("http://dev.preview.kcl.contensis.cloud/study/undergraduate/subject-area/physics")
-        cy.injectAxe();
-    })
-
-   
    it('No detectable ally violations on page load', function() {
-        cy.checkA11y()
+        
+    cy.visit("http://dev.preview.kcl.contensis.cloud/study/undergraduate/subject-area/physics")
+    cy.injectAxe();
+    cy.checkA11y()
     });
 });
