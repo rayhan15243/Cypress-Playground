927e29026233f937386e7b413457dc6814eff45b
Rayhan Ahmed Chowdhury <rayhan15243@gmail.com>
2020-07-17 16:46:00 +0100

    Seperated Desktop and Mobile Validation Code.

--- cypress/integration/cypress-scripts/visualValidationTesting.spec.js
+++ cypress/integration/cypress-scripts/visualValidationTesting.spec.js
@@ -1,25 +1,56 @@
 describe("Visual Validation King'\s Foundation", function(){
        
-        beforeEach(() => {
+  beforeEach(() => {
           cy.visit("http://dev.preview.kcl.contensis.cloud/study/test-foundation-course")
-          cy.eyesOpen({
-            testName: 'King\'s Foundation',
-            appName: 'King\'s Foundation Page',
-            browser: [
-                 { width: 1540, height: 900, name: 'chrome' },
-                 { width: 1540, height: 900, name: 'firefox' },
-                 { width: 1540, height: 900, name: 'safari' },
-                 { width: 1540, height: 900, name: 'ie11' },
-                 { deviceName: 'iPhone X'},
-                 { deviceName: 'Galaxy S5'}
-            ],
-            matchLevel: "Layout"
-           });
-         });
+        });
+
+
+  it("Desktop Visual Validation", function() {
+
+    cy.viewport(1440, 900) 
+
+    cy.eyesOpen({
+     
+      testName: 'King\'s Foundation Page',
+      appName: 'King\'s Foundation Page',
+      browser: [
+           { width: 1440, height: 900, name: 'chrome' },
+           { width: 1440, height: 900, name: 'firefox' },
+           { width: 1440, height: 900, name: 'safari' },
+           { width: 1440, height: 900, name: 'ie11' },
+      ],
+      matchLevel: "Layout"
+   });
+
+   
+
+   cy.eyesCheckWindow({
+    layout: [
+     
+    ],
+      fully: true
+  });
+  
+  });
+  
+      
      
 
-    it('King\'s Foundation', function() {
+    it('Mobile Visual Validation', function() {
+
+     
+      cy.eyesOpen({
+        testName: 'King\'s Foundation Desktop',
+        appName: 'King\'s Foundation Page',
+        browser: [
+             { deviceName: 'iPhone X'},
+             { deviceName: 'Galaxy S5'}
+        ],
+        matchLevel: "Layout"
     
+  });
+
+ 
 
       cy.eyesCheckWindow({
         layout: [
