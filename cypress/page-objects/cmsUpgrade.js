a7883839f43082b16a3cb5df88074500c1e2643c
Rayhan Ahmed Chowdhury <rayhan15243@gmail.com>
2020-07-17 16:05:06 +0100

    Included link to Dev Env and also tidied up code.

--- cypress/page-objects/cmsUpgrade.js
+++ cypress/page-objects/cmsUpgrade.js
@@ -1,21 +1,19 @@
-
+ //cy.get('#ctl35').click({ force: true})
+ // cy.get('form').submit()
 
 export function navigate() {
-    cy.visit("https://cms-kcl-test.cloud.contensis.com/Default.aspx/#")
+   // cy.visit("https://cms-kcl-test.cloud.contensis.com/Default.aspx/#")
+      cy.visit("https://cms-kcl-dev.cloud.contensis.com/Default.aspx")
 }
 
 export function emptyUsername() {
-    // cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
-   // cy.get('#_Textbox_Passwords').type('Password1{enter}',  { force: true })
-   cy.get('#_Textbox_Password').type('Password1', { force: true, log: false })
-   cy.get('#ctl35').click({ force: true})
+   cy.get('#_Textbox_Password').type('Password1{enter}', { force: true, log: false })
    cy.contains('required field')
 }
 
 
 export function emptyPassword() {
-    cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
-    cy.get('#ctl35').click({ force: true})
+    cy.get('#_Textbox_Username').type('rayhantest{enter}', { force: true, log: false})
     cy.contains('Password is a required field')
 }
 
@@ -28,36 +26,27 @@ export function emptyUserPassFields() {
 export function successfulLogin() {
 
     cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
-    // cy.get('#_Textbox_Password').type('Password1{enter}',  { force: true })
-    cy.get('#_Textbox_Password').type('Password1', { force: true, log: false })
-    cy.get('#ctl35').click({ force: true})
-    // cy.get('form').submit()
+    cy.get('#_Textbox_Password').type('Password1{enter}',  { force: true })
+   
 
 }
 
 export function invalidUsername() {
     cy.get('#_Textbox_Username').type('rayhantestt', { force: true, log: false})
-    // cy.get('#_Textbox_Password').type('Password1{enter}',  { force: true })
-    cy.get('#_Textbox_Password').type('Password1', { force: true, log: false })
-     cy.get('#ctl35').click({ force: true})
-     // cy.get('form').submit()
+    cy.get('#_Textbox_Password').type('Password1{enter}', { force: true, log: false })
+
 }
 
 export function invalidPassword() {
 
     cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
- // cy.get('#_Textbox_Password').type('Password1{enter}',  { force: true })
- cy.get('#_Textbox_Password').type('Password11', { force: true, log: false })
-  cy.get('#ctl35').click({ force: true})
-  // cy.get('form').submit()
+    cy.get('#_Textbox_Password').type('Password11{enter}', { force: true, log: false })
+
 }
 
 export function logout() {
     cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
-    // cy.get('#_Textbox_Password').type('Password1{enter}',  { force: true })
-    cy.get('#_Textbox_Password').type('Password1', { force: true, log: false })
-    cy.get('#ctl35').click({ force: true})
-    // cy.get('form').submit()
+    cy.get('#_Textbox_Password').type('Password1{enter}', { force: true, log: false })
     cy.get('#UI_UserToolbar_CurrentUser_hyperlink').click({ force: true})
     cy.get('#cm_logout_item').click({ force: true})   
 }
@@ -65,11 +54,8 @@ export function logout() {
 export function inactiveSession() {
 
     cy.get('#_Textbox_Username').type('rayhantest', { force: true, log: false})
-  // cy.get('#_Textbox_Passwords').type('Password1{enter}',  { force: true })
-  cy.get('#_Textbox_Password').type('Password1', { force: true, log: false })
-  cy.get('#ctl35').click({ force: true})
-  // cy.get('form').submit()
-  cy.get('#UI_UserToolbar_CurrentUser_hyperlink').click({ force: true})
-  cy.get('#cm_logout_item').click({ force: true})
-  cy.go('back') 
+    cy.get('#_Textbox_Password').type('Password1{enter}', { force: true, log: false })
+    cy.get('#UI_UserToolbar_CurrentUser_hyperlink').click({ force: true})
+    cy.get('#cm_logout_item').click({ force: true})
+    cy.go('back') 
 }
