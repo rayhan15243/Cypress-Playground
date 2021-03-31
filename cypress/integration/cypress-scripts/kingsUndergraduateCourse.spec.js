95600c6c503bd842c346018ad44315173d9ee70d
Rayhan Chowdhury (VM) <rayhan.chowdhury@kcl.ac.uk>
2021-03-31 11:10:19 +0100

    Refectored regression script for King's UG Course.

---
+++ cypress/integration/cypress-scripts/kingsUndergraduateCourse.spec.js
@@ -0,0 +1,17 @@
+import { navigate, ugRegression } from "../../page-objects/ugCourse";
+
+
+
+
+describe("King's Undergraduate Course Page Example", function(){
+
+    beforeEach(() => {
+            navigate()
+    })
+
+    it('UG Regression', function() {
+       
+        ugRegression()
+})
+    
+})
--- cypress/page-objects/ugCourse.js
+++ cypress/page-objects/ugCourse.js
@@ -2,106 +2,122 @@
 
 export function navigate() {
    
-    cy.visit("http://dev.preview.kcl.contensis.cloud/study/undergraduate/courses/english-test")
+    // cy.visit("http://dev.preview.kcl.contensis.cloud/study/undergraduate/courses/english-test")
+    cy.visit("https://www.kcl.ac.uk/study/undergraduate/courses/english-ba")
 }
 
-export function assessment() {
-    cy.contains('Teaching & structure').click({ force: true})
-    cy.contains('Assessment')
-    cy.contains('Teaching methods')
-    cy.contains('Learning outcomes')
-    cy.contains('Structure')
-    cy.contains('Required modules')
-    cy.contains('Optional modules')
+export function ugRegression(){
+    cy.get('.course-name').should('have.text', 'English BA')
+    cy.get('.course-category').should('have.text', 'Undergraduate degree')
+    cy.get(':nth-child(1) > [data-testid=TabList]').should('have.text', 'Course essentials')
+    cy.get(':nth-child(2) > [data-testid=TabList]').should('have.text', 'Entry requirements')
+    cy.get(':nth-child(3) > [data-testid=TabList]').should('have.text', 'Teaching & structure')
+    cy.get(':nth-child(4) > [data-testid=TabList]').should('have.text', 'Careers')
+    cy.get(':nth-child(5) > [data-testid=TabList]').should('have.text', 'Fees & Funding')
+    cy.get('[title="Apply via UCAS"]')
+    cy.get('.section-heading')
 }
 
-export function awardingInstitutes() {
-    //  cy.location('protocol').should('eq', 'https:')
-    cy.contains('Awarding Institutions').should('be.visible')
-    // cy.go(-1) cy.go(back)
-    // cy.go(1) cy.go(forward() 
-}
 
-export function baseCampus() {
-    cy.contains('Base campus').should('be.visible')
-}
+// Old test scripts
 
-export function strandCampus() {
-    cy.contains('Strand Campus').should('be.visible')
-    cy.contains('River Thames').should('be.visible')
-    cy.get('[alt="The Quad - Strand campus"]').should('be.visible')
-}
+// export function assessment() {
+//     cy.contains('Teaching & structure').click({ force: true})
+//     cy.contains('Assessment')
+//     cy.contains('Teaching methods')
+//     cy.contains('Learning outcomes')
+//     cy.contains('Structure')
+//     cy.contains('Required modules')
+//     cy.contains('Optional modules')
+// }
 
-export function regulatingBodies() {
+// export function awardingInstitutes() {
+//     //  cy.location('protocol').should('eq', 'https:')
+//     cy.contains('Awarding Institutions').should('be.visible')
+//     // cy.go(-1) cy.go(back)
+//     // cy.go(1) cy.go(forward() 
+// }
+
+// export function baseCampus() {
+//     cy.contains('Base campus').should('be.visible')
+// }
+
+// export function strandCampus() {
+//     cy.contains('Strand Campus').should('be.visible')
+//     cy.contains('River Thames').should('be.visible')
+//     cy.get('[alt="The Quad - Strand campus"]').should('be.visible')
+// }
+
+// export function regulatingBodies() {
         
-    cy.contains('Regulating bodies').should('be.visible')
+//     cy.contains('Regulating bodies').should('be.visible')
 
-}
+// }
 
-export function partners() {
-    cy.contains('Partners').should('be.visible')
-}
+// export function partners() {
+//     cy.contains('Partners').should('be.visible')
+// }
 
-export function additionalCampuses() {
-    cy.contains('Additional campuses ').should('be.visible')
-}
+// export function additionalCampuses() {
+//     cy.contains('Additional campuses ').should('be.visible')
+// }
 
-export function additionalLocation() {
+// export function additionalLocation() {
         
-    cy.contains('Additional locations').should('be.visible')
+//     cy.contains('Additional locations').should('be.visible')
 
-}
+// }
 
-export function awardingInstitutions()  {
+// export function awardingInstitutions()  {
        
-    cy.contains('Awarding Institutions').should('be.visible')
+//     cy.contains('Awarding Institutions').should('be.visible')
     
-}
+// }
 
-export function entryRequirements()  {
+// export function entryRequirements()  {
        
-    cy.contains('Entry requirements').should('be.visible').click()
-    cy.contains('Required grades').should('be.visible')
-    cy.contains('Required subjects').should('be.visible')
-    cy.contains('Preferred subjects').should('be.visible')
+//     cy.contains('Entry requirements').should('be.visible').click()
+//     cy.contains('Required grades').should('be.visible')
+//     cy.contains('Required subjects').should('be.visible')
+//     cy.contains('Preferred subjects').should('be.visible')
     
-}
+// }
 
-export function qualifications() {
-    cy.contains('Entry requirements').should('be.visible').click()
-    cy.get('#altQualifications').select('Australian diploma')
-}
+// export function qualifications() {
+//     cy.contains('Entry requirements').should('be.visible').click()
+//     cy.get('#altQualifications').select('Australian diploma')
+// }
 
-export function internationalBaccalaureate() {
-    cy.contains('Entry requirements').should('be.visible').click()
-    cy.contains('International Baccalaureate').should('be.visible')
-}
+// export function internationalBaccalaureate() {
+//     cy.contains('Entry requirements').should('be.visible').click()
+//     cy.contains('International Baccalaureate').should('be.visible')
+// }
 
-export function gcse() {
-    cy.contains('Entry requirements').should('be.visible').click()
-    cy.contains('GCSEs').should('be.visible')
-}
+// export function gcse() {
+//     cy.contains('Entry requirements').should('be.visible').click()
+//     cy.contains('GCSEs').should('be.visible')
+// }
 
-export function international() {
+// export function international() {
    
-    cy.contains('Entry requirements').should('be.visible').click()
-    cy.get('#intQualifications').select('Brazil')
-    cy.get('#intQualifications').select('New Zealand')
-}
+//     cy.contains('Entry requirements').should('be.visible').click()
+//     cy.get('#intQualifications').select('Brazil')
+//     cy.get('#intQualifications').select('New Zealand')
+// }
 
-export function displayFeesFunding() {
+// export function displayFeesFunding() {
     
-    cy.contains('Fees & Funding')
-    cy.contains('£9,250')
-    cy.contains('Funding')
-    cy.contains('£40000')
-    cy.contains('£5000')
-    cy.contains('Further fee information')
-    cy.contains('Additional costs further information')
-}
+//     cy.contains('Fees & Funding')
+//     cy.contains('£9,250')
+//     cy.contains('Funding')
+//     cy.contains('£40000')
+//     cy.contains('£5000')
+//     cy.contains('Further fee information')
+//     cy.contains('Additional costs further information')
+// }
 
 
-export function getLocationDisclaimerText() {
-    cy.contains('Course essentials').should('be.visible')
-    cy.get('.text > p').should('be.visible')
-}
+// export function getLocationDisclaimerText() {
+//     cy.contains('Course essentials').should('be.visible')
+//     cy.get('.text > p').should('be.visible')
+// }
