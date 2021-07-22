ea23809b208dfe5370bb4964f4a019d7d7776380
Rayhan Chowdhury <rayhan.chowdhury@itoworld.com>
2021-07-22 11:41:10 +0100

    Adding new Script for itoworld

---
+++ cypress/integration/itoworld/bodsInternal.spec.js
@@ -0,0 +1,14 @@
+describe("BODS Internal Homepage", function(){
+
+    it('BODS Internal First Page', function() {
+        cy.visit("https://bodp-internal.itoworld.com/")
+        cy.get(':nth-child(1) > .govuk-list > :nth-child(1) > .govuk-link-bold')
+        .click({ force: true})
+        cy.get('.govuk-grid-column-two-thirds > .govuk-button')
+        .click({ force: true})
+                
+  })
+
+
+
+})
