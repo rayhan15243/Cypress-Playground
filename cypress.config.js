8b64b980310073e526e89922ec408931b2b40880
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-01-15 14:27:02 +0000

    changes to file directory as per 12.3 v

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  pageLoadTimeout: 400000,
+  chromeWebSecurity: false,
+  projectId: 'e53xvv',
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  video: false,
+  env: {
+    MY_ENV_VARIABLE: 'hello',
+  },
+  eyesIsDisabled: false,
+  eyesFailCypressOnDiff: true,
+  eyesPort: 59523,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
